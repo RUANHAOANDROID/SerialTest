2a4abf8946f82438e3e8c9f4a8aedd3e54121ffc
hao88 <52927295@qq.com>
2023-06-11 22:47:41 +0800

    add modbus

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"SerialTest/utils"
 	"bufio"
+	"encoding/hex"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/tarm/serial"
@@ -29,67 +33,94 @@ func pack() {
 }
 func main() {
 	//pack()
-	log.Println("串口初步调试")
+	log.Println("RS485-modbus-variant tools")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("输入COM口名称: ")
+	scanner.Scan()
+	inputCom := scanner.Text()
+	inputCom = strings.ReplaceAll(inputCom, " ", "")
+
+	fmt.Print("输入波特率: ")
+	scanner.Scan()
+	inputBaud := scanner.Text()
+	inputBaud = strings.ReplaceAll(inputBaud, " ", "")
+	baud, err := strconv.Atoi(inputBaud)
+	if err != nil {
+		fmt.Println("转换失败:", baud)
+	} else {
+		fmt.Println("转换结果:", baud)
+	}
 	// 打开串口连接
 	port, err := serial.OpenPort(&serial.Config{
-		Name:        "COM1",      // 请根据实际情况设置COM口名称
-		Baud:        9600,        // 波特率
+		Name:        inputCom,    // 请根据实际情况设置COM口名称
+		Baud:        baud,        // 波特率
 		ReadTimeout: time.Second, // 读取超时时间
 	})
-	log.Printf("串口=COM1 波特率=9600 读超时时间=%s\n", time.Second)
+	log.Printf("串口=%s 波特率=%s 读超时时间=%s\n",
+		inputCom, inputBaud, time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer port.Close()
 
-	// 启动一个goroutine来监听串口数据
-	go listenSerialData(port)
-
 	// 主线程用于发送命令到串口
-	//scanner := bufio.NewScanner(os.Stdin)
+
 	for {
-		//inputCMD(scanner, port)
-		inputAutoTest(port)
+		inputCMD(scanner, port)
+		//inputAutoTest(port)
 	}
 }
 func inputAutoTest(port *serial.Port) {
+	//01 03 00 00 00 0A C5 CD
 	cmd1 := make([]byte, 8)
 	cmd1[0] = 01
 	cmd1[1] = 03
 	cmd1[2] = 00
-	cmd1[3] = 01
-	cmd1[4] = 01
-	cmd1[5] = 01
-	cmd1[6] = 01
-	cmd1[7] = uint8(0xCB)
+	cmd1[3] = 00
+	cmd1[4] = 00
+	cmd1[5] = 0x0A
+	cmd1[6] = 0xC5
+	cmd1[7] = 0xCD
 	status, err := port.Write(cmd1)
-	fmt.Printf("write %d %v \n", status, err)
+	fmt.Printf("write len=%d err=%v \n", status, err)
+	readData(port)
 }
 func inputCMD(scanner *bufio.Scanner, port *serial.Port) {
 	fmt.Print("输入命令: ")
 	scanner.Scan()
-	cmd := scanner.Text()
-
-	writeLen, err := port.Write([]byte(cmd + "\n"))
+	input := scanner.Text()
+	input = strings.ReplaceAll(input, " ", "")
+	cmd, err := hex.DecodeString(input)
+	utils.Log.Println("input cmd->", input)
+	writeLen, err := port.Write(cmd)
 	if err != nil {
 		log.Fatal(err)
 		utils.Log.Println(err.Error())
 	}
 	utils.Log.Println("write->", writeLen, cmd)
+	time.Sleep(time.Second)
+	readData(port)
 }
 
-func listenSerialData(port *serial.Port) {
-	buffer := make([]byte, 8) //避免资源浪费阻塞等待数据落入缓冲区
-	for {
-		n, err := port.Read(buffer)
-		if err != nil {
-			log.Fatalf("读取数据时出错：%v", err)
-		}
-		if n > 0 {
-			log.Printf("已读取 %d 字节数据: %s\n", n, buffer[:n])
-			utils.Log.Println("read <-", fmt.Sprintf("ASCII %c", buffer[:n]))
-			utils.Log.Println("read <-", fmt.Sprintf("HEX %x", buffer[:n]))
-			port.Flush()
-		}
+func readData(port *serial.Port) {
+	buffer := make([]byte, 1024) //避免资源浪费阻塞等待数据落入缓冲区
+	n, err := port.Read(buffer)
+	if err != nil {
+		log.Fatalf("读取数据时出错：%v", err)
 	}
+	data := buffer[:n] // 实际读取到的数据
+	if n > 0 {
+		utils.Log.Println("read(byte) <-", data)
+		hexString := hexSp16(data)
+		utils.Log.Println("byte to hex:", hexString)
+	}
+}
+
+// hexSp16 转换为带空格的16进制
+func hexSp16(data []byte) string {
+	hexString := ""
+	for _, b := range data {
+		hexString += fmt.Sprintf("%02X ", b)
+	}
+	return hexString
 }
